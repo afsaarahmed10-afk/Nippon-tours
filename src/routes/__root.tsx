cf5bad5217991397fc5d9d49029781ffd85ac549
Afsaar Ahmed <afsaarahmed10@gmail.com>
2026-07-14 13:05:51 +0530

    Add Google Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -152,7 +152,24 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VNLVG07VHL"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VNLVG07VHL');
+            `,
+          }}
+        />
       </head>
+
       <body>
         {children}
         <Scripts />
